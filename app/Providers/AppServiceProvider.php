3dd27f79efeddc901fad863ee6c20d174113915d
yorik555 <yorikoTakaki@hotmail.com>
2024-07-05 19:39:29 -0300

    Herokuへのデプロイ準備

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (App::environment(['production'])) {
+            URL::forceScheme('https');
+        }
     }
 }
